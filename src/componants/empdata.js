fbb3810ebcb70597c2d40f72e60254d8953d8d28
atomthink <69460866+atomthink@users.noreply.github.com>
2024-05-25 11:02:58 +0530

    Update empdata.js

--- src/componants/empdata.js
+++ src/componants/empdata.js
@@ -2,4 +2,5 @@ export const Emparr=[
     {id:1,fn:"sarvesh",ln:"indalkar",age:22},
     {id:2,fn:"rahul",ln:"sharma",age:25},
     {id:3,fn:"sachin",ln:"tendulkar",age:40},
+     {id:4,fn:"scout",ln:"op",age:28},
 ]
